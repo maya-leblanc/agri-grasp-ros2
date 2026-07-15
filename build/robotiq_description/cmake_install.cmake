91240ebe714c5868613862daf018bacb58de0b7f
maya-leblanc <stemgirl25@gmail.com>
2026-07-15 10:29:22 -0400

    fix: resolve gravity issues in agri_grasp world
    - Added a static ground plane to the Gazebo simulation.
    - Marked the tree branch model as static to prevent it from falling.
    - Positioned the target orange relative to the branch.

--- build/robotiq_description/cmake_install.cmake
+++ build/robotiq_description/cmake_install.cmake
@@ -43,74 +43,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description" TYPE DIRECTORY FILES
-    "/home/maya/Documents/projects/agri-grasp-ros2/src/ros2_robotiq_gripper/robotiq_description/config"
-    "/home/maya/Documents/projects/agri-grasp-ros2/src/ros2_robotiq_gripper/robotiq_description/launch"
-    "/home/maya/Documents/projects/agri-grasp-ros2/src/ros2_robotiq_gripper/robotiq_description/meshes"
-    "/home/maya/Documents/projects/agri-grasp-ros2/src/ros2_robotiq_gripper/robotiq_description/models"
-    "/home/maya/Documents/projects/agri-grasp-ros2/src/ros2_robotiq_gripper/robotiq_description/rviz"
-    "/home/maya/Documents/projects/agri-grasp-ros2/src/ros2_robotiq_gripper/robotiq_description/urdf"
-    "/home/maya/Documents/projects/agri-grasp-ros2/src/ros2_robotiq_gripper/robotiq_description/worlds"
-    )
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/robotiq_description")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/parent_prefix_path" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_index/share/ament_index/resource_index/parent_prefix_path/robotiq_description")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description/environment" TYPE FILE FILES "/opt/ros/jazzy/share/ament_cmake_core/cmake/environment_hooks/environment/ament_prefix_path.sh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description/environment" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_environment_hooks/ament_prefix_path.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description/environment" TYPE FILE FILES "/opt/ros/jazzy/share/ament_cmake_core/cmake/environment_hooks/environment/path.sh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description/environment" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_environment_hooks/path.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_environment_hooks/local_setup.bash")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_environment_hooks/local_setup.sh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_environment_hooks/local_setup.zsh")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_environment_hooks/local_setup.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_environment_hooks/package.dsv")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/packages" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_index/share/ament_index/resource_index/packages/robotiq_description")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description/cmake" TYPE FILE FILES
-    "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_core/robotiq_descriptionConfig.cmake"
-    "/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_core/robotiq_descriptionConfig-version.cmake"
-    )
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/robotiq_description" TYPE FILE FILES "/home/maya/Documents/projects/agri-grasp-ros2/src/ros2_robotiq_gripper/robotiq_description/package.xml")
+  include("/home/maya/Documents/projects/agri-grasp-ros2/build/robotiq_description/ament_cmake_symlink_install/ament_cmake_symlink_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
